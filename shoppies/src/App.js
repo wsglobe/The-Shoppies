3f6356305e923f759dde00e03c8ba5be6090061b
Walter Wang <wsglobe@gmail.com>
2021-01-07 00:29:14 -0600

    add navbar.js

---
+++ shoppies/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+
+  );
+}
