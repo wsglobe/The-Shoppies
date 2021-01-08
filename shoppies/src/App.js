63993a00191fff9a5a8f784c292724beb7cb4d52
Walter Wang <wsglobe@gmail.com>
2021-01-08 00:33:17 -0600

    add main routers

--- shoppies/src/App.js
+++ shoppies/src/App.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import Search from "./Search.js";
+import Movies from "./Movies.js";
 
 export default function App() {
   return (
     <BrowserRouter>
-    </BrowserRouter>
-
-  );
+    <div className="container">
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route exact path="/search">
+            <Search />
+        </Route>
+        <Route exact path="/movies">
+            <Movies />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 }
