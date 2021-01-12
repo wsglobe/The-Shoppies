ac00dced0a2dbd786c2e49a791270240104b70d7
Walter Wang <wsglobe@gmail.com>
2021-01-11 22:02:41 -0600

    home.js main func

--- shoppies/src/Home.js
+++ shoppies/src/Home.js
@@ -1,2 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="home-layout">
+      <div>
+        <h1>The Shoppies</h1>
+        <p>Movie awards for entrepreneurs</p>
+        <Link to="/search" className="btn btn-default">
+          start searching
+        </Link>
+      </div>
+    </div>
+  );
+}
